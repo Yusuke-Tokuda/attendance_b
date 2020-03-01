91b3023e1c6938773ac8120531545697b5966862
tokudalization@gmail.com <thank-you-very-much@hotmail.co.jp>
2020-03-01 02:32:42 +0000

    full_titled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す
+  def full_titled(page_name ="")
+    base_title = "AttendanceB"
+    if page_name.empty?
+      base_title
+    else
+      page_name + "|" + base_title
+    end
+  end
 end
