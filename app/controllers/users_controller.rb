f25a6e427d7660e5eb67731b418e14b3a7fe45b7
tokudalization@gmail.com <thank-you-very-much@hotmail.co.jp>
2020-03-02 10:15:02 +0000

    サインアップ完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i(show edit update destroy)
   
-  def new
-    @user = User.new
-  end
   
   def show
   end
   
   def index
     @users =User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "アカウントを作成しました。"
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
   end
   
   def edit
@@ -20,4 +31,10 @@ class UsersController < ApplicationController
   
   def destroy
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 100},
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
 end
