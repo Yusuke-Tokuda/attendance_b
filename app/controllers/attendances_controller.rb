38d772e3321d59c3a99df7b1f3dc01d3f154ce3f
tokudalization@gmail.com <thank-you-very-much@hotmail.co.jp>
2020-03-10 02:25:36 +0000

    チュートリアル部分完了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -29,5 +29,22 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
+  
+  private
+   # 一ヶ月分の勤怠情報のストロングパラメーター
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
