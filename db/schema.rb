2785e0646c195b4d3d1eabf600dbf01f44b22a1e
tokudalization@gmail.com <thank-you-very-much@hotmail.co.jp>
2020-03-11 03:02:15 +0000

    追加8まで

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20200308131832) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-03-08 23:00:00"
-    t.datetime "work_time", default: "2020-03-08 22:30:00"
+    t.datetime "basic_time", default: "2020-03-11 03:00:00"
+    t.datetime "work_time", default: "2020-03-10 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,10 @@ User.create!(name: "ムツゴロウ御大",
              email: "sample@email.com",
              password: "password",
              password_confirmation: "password",
+             department: "セクシーコマンドー部",
              admin: true)
 
-60.times do |n|
+69.times do |n|
   name  = Faker::Creature::Animal.name
   email = "sample-#{n+1}@email.com"
   password = "password"
